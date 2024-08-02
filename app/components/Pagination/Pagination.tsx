1b1f43b526e531905d1320167f4d5b72dfe11396
Taras Maltsev <taras.maltsev@gmail.com>
2024-08-02 20:35:47 +0300

    base pagination feature is integrated with laravel api

--- app/components/Pagination/Pagination.tsx
+++ app/components/Pagination/Pagination.tsx
@@ -16,93 +16,137 @@ import {
 } from "./Pagination.styled";
 import { SvgContainer } from "@/app/styles/common.styled";
 import { poppins } from "@/app/assets/fonts";
+import { EventsLinks, EventsMeta } from "@/app/types/types";
+import eventsStore from "@/app/mobX/stores/eventsStore";
+import { toast } from "react-toastify";
+import { createURL } from "@/app/services/createURL";
 
 const shouldForwardProp = (prop: string) => prop !== "isActive";
 
-export const Pagination: FC = observer((): JSX.Element => {
-  const [pagination, setPagination] = useState<(number | string)[]>();
-  const searchParams = useSearchParams();
-  const { paginationStore } = useStore();
+interface PaginationProps {
+  meta: EventsMeta | null;
+  error: string | null;
+}
 
-  const router = useRouter();
-  const pageFromURL = searchParams.get("page");
+export const Pagination: FC<PaginationProps> = observer(
+  ({ meta, error }): JSX.Element => {
+    const [currentPage, setCurrentPage] = useState<number | null>(
+      meta && meta.current_page
+    );
+    const [prevPage, setPrevPage] = useState<number | null>(1);
+    const [nextPage, setNextPage] = useState<number | null>(2);
+    const [lastPage, setLastPage] = useState<number | null>(
+      meta && meta.last_page
+    );
+    const [pagination, setPagination] = useState<(number | "...")[] | null>();
+    const searchParams = useSearchParams();
+    const { paginationStore } = useStore();
 
-  // console.log(pageFromURL);
+    const router = useRouter();
+    const pageFromURL = searchParams.get("page");
 
-  const currentPage = paginationStore.currentPage;
-  const totalPages = paginationStore.totalPages;
-  const eventsPerPage = paginationStore.eventsPerPage;
-  const currentEventsAmount = totalPages * eventsPerPage;
+    useEffect(() => {
+      if (meta) paginationStore.setPaginationData(meta);
+      if (error) toast.error(error);
+    }, []);
 
-  useEffect(() => {
-    // if (pageFromURL && Number(pageFromURL) !== 1 && currentPage === 1) {
-    //   router.push(`?page=${pageFromURL}`);
-    //   paginationStore.updateCurrentPage(Number(pageFromURL));
-    // } else {
-    //   currentPage && currentEventsAmount > eventsPerPage
-    //     ? router.push(`?page=${currentPage}`)
-    //     : router.push("");
-    // }
-    setPagination(paginationStore.pagination);
-  }, [
-    // pageFromURL,
-    // currentPage,
-    // currentEventsAmount,
-    // eventsPerPage,
-    paginationStore.pagination,
-  ]);
+    useEffect(() => {
+      const { currentPage, nextPage, prevPage, lastPage, pagination } =
+        paginationStore;
+      setCurrentPage(currentPage);
+      setPrevPage(prevPage);
+      setNextPage(nextPage);
+      setLastPage(lastPage);
+      setPagination(pagination);
+    }, [
+      paginationStore.nextPage,
+      paginationStore.prevPage,
+      paginationStore.currentPage,
+      paginationStore.lastPage,
+      paginationStore.pagination,
+    ]);
 
-  const onPageClick = (page: string | number): void => {
-    if (typeof page === "number" && currentPage !== page) {
-      paginationStore.updateCurrentPage(page);
-    }
-  };
+    // const currentPage = paginationStore.currentPage;
+    // const totalPages = paginationStore.totalPages;
+    // const eventsPerPage = paginationStore.eventsPerPage;
+    // const currentEventsAmount = totalPages * eventsPerPage;
 
-  return (
-    <StyleSheetManager shouldForwardProp={shouldForwardProp}>
-      {currentEventsAmount > eventsPerPage ? (
-        <PagesList>
-          <PageItem
-            isActive={false}
-            onClick={() => {
-              if (currentPage === 1) return;
-              paginationStore.updateCurrentPage(currentPage - 1);
-            }}
-            className={poppins.className}
-          >
-            <SvgContainer>
-              <SvgDecreasePage isActive={currentPage > 1} />
-            </SvgContainer>
-          </PageItem>
-          {pagination &&
-            pagination.map((page) => {
-              return (
-                <PageItem
-                  key={nanoid()}
-                  onClick={() => onPageClick(page)}
-                  isActive={page === currentPage}
-                >
-                  {typeof page === "number" ? (
-                    <Page>{page}</Page>
-                  ) : (
-                    <Ellipsis>{page}</Ellipsis>
-                  )}
-                </PageItem>
-              );
-            })}
-          <PageItem
-            isActive={false}
-            onClick={() => {
-              if (currentPage === totalPages) return;
-              paginationStore.updateCurrentPage(currentPage + 1);
-            }}
-          >
-            <SvgIncreasePage isActive={currentPage !== totalPages} />
-          </PageItem>
-        </PagesList>
-      ) : (
-        <Fragment />
-      )}
-    </StyleSheetManager>
-  );
-});
+    // useEffect(() => {
+    //   if (pageFromURL && Number(pageFromURL) !== 1 && currentPage === 1) {
+    //     router.push(`?page=${pageFromURL}`);
+    //     paginationStore.setCurrentPage(Number(pageFromURL));
+    //   } else {
+    //     currentPage && currentEventsAmount > eventsPerPage
+    //       ? router.push(`?page=${currentPage}`)
+    //       : router.push("");
+    //   }
+    //   setPagination(paginationStore.pagination);
+    // }, [
+    //   pageFromURL,
+    //   currentPage,
+    //   currentEventsAmount,
+    //   eventsPerPage,
+    //   paginationStore.pagination,
+    // ]);
+
+    return (
+      <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+        {lastPage && lastPage >= 1 ? (
+          <PagesList>
+            <PageItem
+              isActive={currentPage !== 1}
+              onClick={() => {
+                if (currentPage === prevPage) return;
+                if (prevPage) {
+                  eventsStore.getEvents(createURL({ page: prevPage }));
+                }
+              }}
+              className={poppins.className}
+            >
+              <SvgContainer>
+                <SvgDecreasePage
+                  isActive={
+                    currentPage !== null &&
+                    currentPage !== undefined &&
+                    currentPage > 1
+                  }
+                />
+              </SvgContainer>
+            </PageItem>
+            {pagination &&
+              pagination.map((page) => {
+                return (
+                  <PageItem
+                    key={nanoid()}
+                    onClick={() => {
+                      if (typeof page === "number")
+                        eventsStore.getEvents(createURL({ page: page }));
+                    }}
+                    isActive={page === currentPage}
+                  >
+                    {typeof page === "number" ? (
+                      <Page>{page}</Page>
+                    ) : (
+                      <Ellipsis>{page}</Ellipsis>
+                    )}
+                  </PageItem>
+                );
+              })}
+            <PageItem
+              isActive={currentPage !== lastPage}
+              onClick={() => {
+                if (currentPage === lastPage) return;
+                if (nextPage)
+                  eventsStore.getEvents(createURL({ page: nextPage }));
+              }}
+            >
+              <SvgIncreasePage isActive={currentPage !== lastPage} />
+            </PageItem>
+          </PagesList>
+        ) : (
+          <Fragment />
+        )}
+      </StyleSheetManager>
+    );
+  }
+);
--- app/mobX/stores/paginationStore.tsx
+++ app/mobX/stores/paginationStore.tsx
@@ -1,80 +1,72 @@
 import { observable, action, makeAutoObservable } from "mobx";
-import { NewEvent } from "../../types/types";
+import type { EventsMeta } from "../../types/types";
 
 class PaginationStore {
   @observable
-  currentPage: number = 1;
-  eventsPerPage: number = 1;
-  totalPages: number = 1;
-  pagination: (number | string)[] = [];
+  currentPage: number | null = null;
+  prevPage: number | null = null;
+  nextPage: number | null = null;
+  lastPage: number | null = null;
+  eventsPerPage: number | null = null;
+  totalEvents: number | null = null;
+  pagination: (number | "...")[] | null = null;
 
   constructor() {
     makeAutoObservable(this);
   }
 
   @action
-  updatePagination(events: NewEvent[]): void {
-    this.totalPages = Math.ceil(events.length / this.eventsPerPage);
-    const ellipsis = "...";
-    this.pagination = [];
+  setPagination(lastPage: number | null, currentPage: number | null): void {
+    if (lastPage && currentPage) {
+      const maxPagesToShow = 3;
+      const pages: (number | "...")[] = [];
 
-    for (let i = 1; i <= this.totalPages; i += 1) {
-      if (
-        (i <= 3 && this.currentPage < 3) ||
-        (i <= 4 && this.currentPage === 3) ||
-        (i <= 5 && this.totalPages <= 5) ||
-        (i >= this.totalPages - 2 && this.currentPage >= this.totalPages - 2)
-      ) {
-        this.pagination.push(i);
+      let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
+      let endPage = Math.min(
+        lastPage,
+        currentPage + Math.floor(maxPagesToShow / 2)
+      );
+
+      if (endPage - startPage < maxPagesToShow - 1) {
+        if (endPage === lastPage) {
+          startPage = Math.max(1, endPage - (maxPagesToShow - 1));
+        } else if (startPage === 1) {
+          endPage = Math.min(lastPage, startPage + (maxPagesToShow - 1));
+        }
       }
 
-      if (
-        this.totalPages > 5 &&
-        i === this.totalPages - 2 &&
-        this.currentPage === this.totalPages - 2
-      ) {
-        this.pagination.unshift(this.currentPage - 1);
+      if (startPage > 1) {
+        pages.push(1);
+        if (startPage > 2) pages.push("...");
       }
 
-      if (
-        this.totalPages > 4 &&
-        this.currentPage >= 4 &&
-        this.currentPage <= this.totalPages - 3 &&
-        this.pagination.length < 1
-      ) {
-        this.pagination.push(
-          this.currentPage - 1,
-          this.currentPage,
-          this.currentPage + 1
-        );
+      for (let i = startPage; i <= endPage; i++) {
+        pages.push(i);
       }
-    }
 
-    if (this.totalPages >= 6 && this.currentPage <= this.totalPages - 3) {
-      this.pagination.push(ellipsis, this.totalPages);
-    }
+      if (endPage < lastPage) {
+        if (endPage < lastPage - 1) pages.push("...");
+        pages.push(lastPage);
+      }
 
-    if (
-      this.totalPages > 5 &&
-      this.currentPage >= 4 &&
-      this.currentPage <= this.totalPages
-    ) {
-      this.pagination.unshift(1, ellipsis);
+      this.pagination = pages;
+    } else {
+      this.pagination = null;
     }
   }
 
   @action
-  updateCurrentPage(page: number): void {
-    this.currentPage = page;
-  }
-
-  @action
-  displayEventsPerPage(events: NewEvent[]): NewEvent[] {
-    const startIndex = (this.currentPage - 1) * this.eventsPerPage;
-    const endIndex = startIndex + this.eventsPerPage;
-    const displayedEventsPerPage = events.slice(startIndex, endIndex);
-    this.updatePagination(events);
-    return displayedEventsPerPage;
+  setPaginationData(data: EventsMeta): void {
+    if (data) {
+      const { current_page, last_page, per_page, total } = data;
+      this.currentPage = current_page;
+      this.prevPage = current_page > 1 ? current_page - 1 : 1;
+      this.nextPage = current_page < last_page ? current_page + 1 : last_page;
+      this.lastPage = last_page;
+      this.eventsPerPage = per_page;
+      this.totalEvents = total;
+      this.setPagination(last_page, current_page);
+    }
   }
 }
 
