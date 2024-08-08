1bea20e770fcf86abad70d6e994edf9afec8abc0
Taras Maltsev <taras.maltsev@gmail.com>
2024-08-08 19:55:31 +0300

    home page is  updated

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,9 @@
 import { Suspense } from "react";
-
 import { AppBar } from "../components/AppBar/AppBar";
 import EventsList from "../components/EventsList/EventsList";
 import { Pagination } from "../components/Pagination/Pagination";
 import Loading from "../loading";
-import { EventsProps } from "../types/types";
 import { getEvents } from "./actions";
-import { useSearchParams } from "next/navigation";
 
 interface PageProps {
   searchParams: string;
@@ -14,8 +11,6 @@ interface PageProps {
 
 const Home = async ({ searchParams }: PageProps): Promise<JSX.Element> => {
   const queryString = new URLSearchParams(searchParams).toString();
-
-  console.log("queryString: ", queryString);
 
   const { eventsData, error } = await getEvents(queryString);
 
