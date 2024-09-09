09f61c7e4630b6fd628b8d53616a9857a3b726ac
Taras Maltsev <taras.maltsev@gmail.com>
2024-09-09 15:45:53 +0300

    types are updated

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,18 +1,52 @@
 export interface NewEvent {
-  id: string;
+  id?: string | undefined;
   title: string;
-  description: string;
+  description?: string;
   date: string;
   time: string;
-  location: string;
-  category: string;
-  image: string;
-  priority: string;
-  [key: string]: string;
+  location?: string;
+  category?: string;
+  picture?: string | File | Blob;
+  priority?: string;
+  [key: string]: string | File | Blob | undefined;
 }
 
 export interface User {
   name?: string;
   email: string;
-  password: string;
+  password: string | null;
+}
+
+export interface EventsLinks {
+  first: string;
+  last: string;
+  next: string;
+}
+
+export interface PageLinks {
+  active: boolean;
+  label: string;
+  url: string | null;
+}
+
+export interface EventsMeta {
+  current_page: number;
+  last_page: number;
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  links: PageLinks[];
+}
+
+export interface EventsProps {
+  data: NewEvent[];
+  meta: EventsMeta;
+  links: EventsLinks;
+}
+
+export interface ChangePassworProps {
+  current_password?: string;
+  new_password: string;
+  new_password_confirmation: string;
 }
