796c2a4d52c6549f4d92feb8b5be9d7d2ec4af77
Taras Maltsev <taras.maltsev@gmail.com>
2024-10-11 14:00:32 +0300

    start page is updated

---
+++ app/components/StartPageContainer/StartPageContainer.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import React, { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { poppins } from "@/app/assets/fonts";
+import {
+  Container,
+  CreateIcon,
+  DeleteIcon,
+  EditIcon,
+  FeatureDescription,
+  FeatureItem,
+  FeaturesList,
+  FeaturesListTitle,
+  FeatureTitle,
+  FilterIcon,
+  IconWrapper,
+  SearchIcon,
+  SortIcon,
+  Subtitle,
+  Title,
+} from "./StartPageContainer.styled";
+
+export const StartPageContainer: FC = (): JSX.Element => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Container className={poppins.className}>
+        <Title>{t("startPage.title")}</Title>
+        <Subtitle>{t("startPage.subtitle")}</Subtitle>
+      </Container>
+      <Container className={poppins.className}>
+        <FeaturesListTitle>{t("startPage.featureTitle")}</FeaturesListTitle>
+        <FeaturesList>
+          <FeatureItem>
+            <FeatureTitle>
+              <IconWrapper>
+                <CreateIcon />
+              </IconWrapper>
+              {t("startPage.creation")}
+            </FeatureTitle>
+            <FeatureDescription>
+              {t("startPage.creationDescription")}
+            </FeatureDescription>
+          </FeatureItem>
+          <FeatureItem>
+            <FeatureTitle>
+              <IconWrapper>
+                <EditIcon />
+              </IconWrapper>
+              {t("startPage.editing")}
+            </FeatureTitle>
+            <FeatureDescription>
+              {t("startPage.editingDescription")}
+            </FeatureDescription>
+          </FeatureItem>
+          <FeatureItem>
+            <FeatureTitle>
+              <IconWrapper>
+                <SortIcon />
+              </IconWrapper>
+              {t("startPage.sorting")}
+            </FeatureTitle>
+            <FeatureDescription>
+              {t("startPage.sortingDescription")}
+            </FeatureDescription>
+          </FeatureItem>
+          <FeatureItem>
+            <FeatureTitle>
+              <IconWrapper>
+                <SearchIcon />
+              </IconWrapper>
+              {t("startPage.searching")}
+            </FeatureTitle>
+            <FeatureDescription>
+              {t("startPage.searchingDescription")}
+            </FeatureDescription>
+          </FeatureItem>
+          <FeatureItem>
+            <FeatureTitle>
+              <IconWrapper>
+                <FilterIcon />
+              </IconWrapper>
+              {t("startPage.filtration")}
+            </FeatureTitle>
+            <FeatureDescription>
+              {t("startPage.filtrationDescription")}
+            </FeatureDescription>
+          </FeatureItem>
+          <FeatureItem>
+            <FeatureTitle>
+              <IconWrapper>
+                <DeleteIcon />
+              </IconWrapper>
+              {t("startPage.deletion")}
+            </FeatureTitle>
+            <FeatureDescription>
+              {t("startPage.deletionDescription")}
+            </FeatureDescription>
+          </FeatureItem>
+        </FeaturesList>
+      </Container>
+    </>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,107 +1,13 @@
-"use client";
+"use server";
 
 import { Suspense } from "react";
 import Loading from "./loading";
-import {
-  Container,
-  FeaturesList,
-  FeatureItem,
-  FeaturesListTitle,
-  FeatureTitle,
-  Subtitle,
-  Title,
-  FeatureDescription,
-  CreateIcon,
-  IconWrapper,
-  EditIcon,
-  SortIcon,
-  FilterIcon,
-  SearchIcon,
-  DeleteIcon,
-} from "./page.styled";
-import { poppins } from "./assets/fonts";
-import { useTranslation } from "react-i18next";
+import { StartPageContainer } from "./components/StartPageContainer/StartPageContainer";
 
 const StartPage = (): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
     <Suspense fallback={<Loading />}>
-      <Container className={poppins.className}>
-        <Title>{t("startPage.title")}</Title>
-        <Subtitle>{t("startPage.subtitle")}</Subtitle>
-      </Container>
-      <Container className={poppins.className}>
-        <FeaturesListTitle>{t("startPage.featureTitle")}</FeaturesListTitle>
-        <FeaturesList>
-          <FeatureItem>
-            <FeatureTitle>
-              <IconWrapper>
-                <CreateIcon />
-              </IconWrapper>
-              {t("startPage.creation")}
-            </FeatureTitle>
-            <FeatureDescription>
-              {t("startPage.creationDescription")}
-            </FeatureDescription>
-          </FeatureItem>
-          <FeatureItem>
-            <FeatureTitle>
-              <IconWrapper>
-                <EditIcon />
-              </IconWrapper>
-              {t("startPage.editing")}
-            </FeatureTitle>
-            <FeatureDescription>
-              {t("startPage.editingDescription")}
-            </FeatureDescription>
-          </FeatureItem>
-          <FeatureItem>
-            <FeatureTitle>
-              <IconWrapper>
-                <SortIcon />
-              </IconWrapper>
-              {t("startPage.sorting")}
-            </FeatureTitle>
-            <FeatureDescription>
-              {t("startPage.sortingDescription")}
-            </FeatureDescription>
-          </FeatureItem>
-          <FeatureItem>
-            <FeatureTitle>
-              <IconWrapper>
-                <SearchIcon />
-              </IconWrapper>
-              {t("startPage.searching")}
-            </FeatureTitle>
-            <FeatureDescription>
-              {t("startPage.searchingDescription")}
-            </FeatureDescription>
-          </FeatureItem>
-          <FeatureItem>
-            <FeatureTitle>
-              <IconWrapper>
-                <FilterIcon />
-              </IconWrapper>
-              {t("startPage.filtration")}
-            </FeatureTitle>
-            <FeatureDescription>
-              {t("startPage.filtrationDescription")}
-            </FeatureDescription>
-          </FeatureItem>
-          <FeatureItem>
-            <FeatureTitle>
-              <IconWrapper>
-                <DeleteIcon />
-              </IconWrapper>
-              {t("startPage.deletion")}
-            </FeatureTitle>
-            <FeatureDescription>
-              {t("startPage.deletionDescription")}
-            </FeatureDescription>
-          </FeatureItem>
-        </FeaturesList>
-      </Container>
+      <StartPageContainer />
     </Suspense>
   );
 };
