a7da367340cba7a34fd182c52f14b3182a58dbe0
Taras Maltsev <taras.maltsev@gmail.com>
2024-10-22 19:58:45 +0300

    google login is fixed

---
+++ app/components/LoggedIn/LoggedIn.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { FC, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/navigation";
+import { useStore } from "@/app/mobX/useStore";
+import { toast } from "react-toastify";
+import { localizeResponses } from "@/app/services/localizeResponses";
+
+interface LoggedInProps {
+  token?: string | undefined;
+  error?: string | null;
+}
+
+const LoggedIn: FC<LoggedInProps> = ({ token, error }) => {
+  const router = useRouter();
+  const { t, i18n } = useTranslation();
+  const { authStore } = useStore();
+
+  useEffect(() => {
+    const { setLoggedIn, setToken, deleteToken, setError } = authStore;
+
+    if (!authStore.isLoggedIn && !authStore.token && token) {
+      setToken(token);
+      setLoggedIn(true);
+    }
+
+    if (error === "Unauthenticated.") {
+      toast.error(t(localizeResponses(error)));
+      setError(error);
+      deleteToken();
+      setLoggedIn(false);
+      router.push(`/?lang=${i18n.language}`);
+      return;
+    }
+  }, [
+    authStore.isLoggedIn,
+    authStore.token,
+    token,
+    error,
+    toast.error,
+    t,
+    router.push,
+  ]);
+
+  return null;
+};
+
+export default LoggedIn;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,14 @@
 "use server";
 
 import { Suspense } from "react";
+import { cookies } from "next/headers";
 import { AppBar } from "../components/AppBar/AppBar";
 import EventsList from "../components/EventsList/EventsList";
 import { Pagination } from "../components/Pagination/Pagination";
 import Loading from "../loading";
 import { getEvents } from "./actions";
 import UpdateURLWithQueryParams from "../components/UpdateURLWithQueryParams/UpdateURLWithQueryParams";
+import LoggedIn from "../components/LoggedIn/LoggedIn";
 
 interface PageProps {
   searchParams: string;
@@ -14,11 +16,12 @@ interface PageProps {
 
 const Home = async ({ searchParams }: PageProps): Promise<JSX.Element> => {
   const queryString = new URLSearchParams(searchParams).toString();
-
+  const token = cookies().get("token")?.value;
   const { eventsList, error } = await getEvents(queryString);
 
   return (
     <Suspense fallback={<Loading />}>
+      <LoggedIn token={token} error={error} />
       <UpdateURLWithQueryParams />
       <AppBar />
       <EventsList eventsList={eventsList && eventsList.data} error={error} />
