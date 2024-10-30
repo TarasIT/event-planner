6fe7e0c56c266f7ddb4200b9a2c6df7ff58f6b81
Taras Maltsev <taras.maltsev@gmail.com>
2024-10-30 20:58:21 +0200

    back link for auth paths is updated

--- app/components/BackLink/BackLink.tsx
+++ app/components/BackLink/BackLink.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { FC } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useTranslation } from "react-i18next";
 import { GoBackLink, SvgBackLinkIcon, Back } from "./BackLink.styled";
 import { poppins } from "@/app/assets/fonts";
@@ -9,12 +9,24 @@ import { poppins } from "@/app/assets/fonts";
 export const BackLink: FC = (): JSX.Element => {
   const { t } = useTranslation();
   const router = useRouter();
+  const pathname = usePathname();
+
+  const pathRedirects: Record<string, string> = {
+    "/signup": "/",
+    "/login": "/",
+    "/forgot-password": "/login",
+  };
 
   return (
     <GoBackLink
       onClick={() => {
-        router.back();
-        router.refresh();
+        const redirectPath = pathRedirects[pathname];
+        if (redirectPath) {
+          router.push(redirectPath);
+        } else {
+          router.back();
+          router.refresh();
+        }
       }}
       className={poppins.className}
     >
