2e4a41db71ed2fcd8bc77ff442b0c8f4e539dd85
Taras Maltsev <taras.maltsev@gmail.com>
2024-10-31 14:24:52 +0200

    unnecessarry checking is removed

--- middlewares/authCheckMiddleware.ts
+++ middlewares/authCheckMiddleware.ts
@@ -2,16 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { protectedPaths } from "../app/data/protectedPaths";
 
 export function authCheckMiddleware(request: NextRequest, response: NextResponse): NextResponse {
-  const startUrl = new URL("/", request.url);
   const homeUrl = new URL("/home", request.url);
   const pathname = request.nextUrl.pathname;
   const isProtectedPath = protectedPaths.some((path) => pathname.startsWith(path));
 
   const token = request.cookies.get("token");
 
-  if (isProtectedPath && !token) {
-    response = NextResponse.redirect(startUrl);
-  }
   if (token && !isProtectedPath) {
     response = NextResponse.redirect(homeUrl);
   }
